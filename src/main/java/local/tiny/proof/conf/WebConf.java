296da0ca04c77e9efa0630febc7667c18a24b56b
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-01-18 21:52:00 +0700

    Update WebConf.java

--- src/main/java/local/tiny/proof/conf/WebConf.java
+++ src/main/java/local/tiny/proof/conf/WebConf.java
@@ -39,6 +39,7 @@ public class WebConf
         registry.addViewController("/accordion-panel").setViewName("forward:/accordion-panel.xhtml");
         registry.addViewController("/data-list").setViewName("forward:/data-list.xhtml");
         registry.addViewController("/table").setViewName("forward:/table.xhtml");
+        registry.addViewController("/table-with-highlighted-lines").setViewName("forward:/table-with-highlighted-lines.xhtml");
         registry.addViewController("/bar-chart").setViewName("forward:/bar-chart.xhtml");
         registry.addViewController("/line-chart").setViewName("forward:/line-chart.xhtml");
         registry.addViewController("/candlestick-chart").setViewName("forward:/candlestick-chart.xhtml");
