d0c7e4ecb8fa3585f63be6ea13a1944cef7b14c8
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-01-19 19:53:59 +0700

    Update WebConf.java

--- src/main/java/local/tiny/proof/conf/WebConf.java
+++ src/main/java/local/tiny/proof/conf/WebConf.java
@@ -41,6 +41,7 @@ public class WebConf
         registry.addViewController("/table").setViewName("forward:/table.xhtml");
         registry.addViewController("/table-with-highlighted-lines").setViewName("forward:/table-with-highlighted-lines.xhtml");
         registry.addViewController("/table-with-right-align-values").setViewName("forward:/table-with-right-align-values.xhtml");
+        registry.addViewController("/table-toggle").setViewName("forward:/table-toggle.xhtml");
         registry.addViewController("/table-with-sortable-values").setViewName("forward:/table-with-sortable-values.xhtml");
         registry.addViewController("/bar-chart").setViewName("forward:/bar-chart.xhtml");
         registry.addViewController("/line-chart").setViewName("forward:/line-chart.xhtml");
